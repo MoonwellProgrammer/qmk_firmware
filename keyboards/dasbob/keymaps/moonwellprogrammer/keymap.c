21e4d7d9f5203f86b43acdda8e220fe761b3f26f
moonwellprogrammer <moonwell@protonmail.com>
2024-04-11 00:00:10 -0300

    fixed mod tap key issue

--- keyboards/dasbob/keymaps/moonwellprogrammer/keymap.c
+++ keyboards/dasbob/keymaps/moonwellprogrammer/keymap.c
@@ -51,6 +51,7 @@ enum layers {
 
 enum keycodes {
     // Custom oneshot mod implementation with no timers (oneshot.c)
+
     OS_SHFT = SAFE_RANGE,
     OS_CTRL,
     OS_ALT,
@@ -65,7 +66,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
         KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
         KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
-                          KC_BSPC, KC_LSFT, LA_NAV,  LA_SYM,  KC_SPC,  MT(KC_RCTL,KC_ENT)
+                          KC_BSPC, KC_LSFT, LA_NAV,  LA_SYM,  KC_SPC,  MT(MOD_RCTL,KC_ENT)
     ),
 
     [_SYM] = LAYOUT_split_3x5_3(
