9b2e690438efb227d9ad426aae60b4aafca99e52
moonwellprogrammer <moonwell@protonmail.com>
2024-04-10 23:45:06 -0300

    minor keymap edits, mt on enter key (added ctrl)

--- keyboards/dasbob/keymaps/moonwellprogrammer/keymap.c
+++ keyboards/dasbob/keymaps/moonwellprogrammer/keymap.c
@@ -65,19 +65,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
         KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
         KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
-                          KC_BSPC, KC_LSFT, LA_NAV,  LA_SYM,  KC_SPC,  KC_ENT
+                          KC_BSPC, KC_LSFT, LA_NAV,  LA_SYM,  KC_SPC,  MT(KC_RCTL,KC_ENT)
     ),
 
     [_SYM] = LAYOUT_split_3x5_3(
         KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,                                           
-        KC_TILD, KC_GRV,  KC_QUOT, KC_DQUO, KC_PMNS, KC_PLUS, OS_SHFT, OS_CTRL, OS_ALT,  OS_GUI,
+        KC_TILD, KC_GRV,  KC_QUOT, KC_DQUO, KC_MINS, KC_PLUS, OS_SHFT, OS_CTRL, OS_ALT,  OS_GUI,
         KC_BSLS, KC_PIPE, LA_MED,  XXXXXXX, KC_UNDS, KC_EQL,  KC_LCBR, KC_RCBR, KC_LBRC, KC_RBRC,
                           _______, _______, _______, _______, _______, _______
     ),
 
     [_NAV] = LAYOUT_split_3x5_3(
-        SW_WIN,  SW_TAB,  TAB_L,   TAB_R,   KC_ESC,  KC_ESC,  KC_HOME, KC_END, KC_BSPC, KC_DEL,
-        OS_GUI,  OS_ALT,  OS_CTRL, OS_SHFT, KC_ENT,  KC_LEFT, KC_DOWN, KC_UP,  KC_RGHT, KC_ENT,
+        SW_WIN,  SW_TAB,  TAB_L,   TAB_R,   KC_ENT,  KC_ESC,  KC_HOME, KC_END, KC_BSPC, KC_DEL,
+        OS_GUI,  OS_ALT,  OS_CTRL, OS_SHFT, KC_ESC,  KC_LEFT, KC_DOWN, KC_UP,  KC_RGHT, KC_ENT,
         DESK_L,  DESK_R,  LA_MOU,  KC_PSCR, KC_TAB,  KC_TAB,  KC_PGDN, KC_PGUP,XXXXXXX, KC_CAPS,
                           _______, _______, _______, _______, _______, _______
     ),
