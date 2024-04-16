6383376daa6b812d2852646b0734f22bc037f89a
moonwellprogrammer <moonwell@protonmail.com>
2024-04-16 11:08:41 -0300

    added mod tap keys to thumb cluster

--- keyboards/dasbob/keymaps/moonwellprogrammer/keymap.c
+++ keyboards/dasbob/keymaps/moonwellprogrammer/keymap.c
@@ -3,9 +3,6 @@
 #include "oneshot.h"
 #include "swapper.h"
 
-#define FWD G(KC_RBRC) // possibly remove
-#define BACK G(KC_LBRC) // possibly remove
-
 #define TAB_L C(S(KC_TAB))
 #define TAB_R C(KC_TAB)
 
@@ -56,7 +53,6 @@ enum keycodes {
     OS_CTRL,
     OS_ALT,
     OS_GUI,
-    // Custom swapper implementation (swapper.c)
     SW_WIN, // Switch to next window         (alt-tab)
     SW_TAB, // Switch to next tab            (ctrl-tab)
 };
@@ -66,7 +62,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
         KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
         KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
-                          KC_BSPC, KC_LSFT, LA_NAV,  LA_SYM,  KC_SPC,  MT(MOD_RCTL,KC_ENT)
+              RALT_T(KC_BSPC), KC_LSFT, LA_NAV,  LA_SYM,  RSFT_T(KC_SPC),  RCTL_T(KC_ENT) 
+              // The thumb keys are using mod tap shorthand to make things more compact in the keymap
+              // RCTL_T(KC_ENT) is equivalent to MT(MOD_RCTL, KC_ENT) for example
     ),
 
     [_SYM] = LAYOUT_split_3x5_3(
