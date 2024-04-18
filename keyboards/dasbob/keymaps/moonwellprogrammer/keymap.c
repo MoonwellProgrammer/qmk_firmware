3fc0ca4665b36e5e923a54f41bdd8362db01774f
moonwellprogrammer <moonwell@protonmail.com>
2024-04-18 11:35:27 -0300

    minor edits

--- keyboards/dasbob/keymaps/moonwellprogrammer/keymap.c
+++ keyboards/dasbob/keymaps/moonwellprogrammer/keymap.c
@@ -63,8 +63,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
         KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
               RALT_T(KC_BSPC), KC_LSFT, LA_NAV,  LA_SYM,  RSFT_T(KC_SPC),  RCTL_T(KC_ENT) 
-              // The thumb keys are using mod tap shorthand to make things more compact in the keymap
-              // RCTL_T(KC_ENT) is equivalent to MT(MOD_RCTL, KC_ENT) for example
+              // The thumb keys are using 'mod tap' shorthand to make things more compact in the keymap
+              // ex: RCTL_T(KC_ENT) is equivalent to MT(MOD_RCTL, KC_ENT)    
     ),
 
     [_SYM] = LAYOUT_split_3x5_3(
